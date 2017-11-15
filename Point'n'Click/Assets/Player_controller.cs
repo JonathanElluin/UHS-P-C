5098ef34684bea7af558e97217c7c5766f593cb2
Florent <boissy.florent@gmail.com>
2017-11-15 09:04:16 +0100

    Commit

---
+++ Point'n'Click/Assets/Player_controller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour {
+
+    private Animator playerAnimator;
+    private SpriteRenderer spriteRenderer;
+    public GameObject Self;
+    private int i = 0;
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+        playerAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+    private void Update()
+    {
+        if(Time.realtimeSinceStartup <= 8 && Time.realtimeSinceStartup >= 3)
+        {
+            playerAnimator.SetInteger("Speed", 1);
+            spriteRenderer.flipX = true;
+            transform.Translate(Vector2.left * Speed * Time.deltaTime);
+        }
+        else
+        {
+            playerAnimator.SetInteger("Speed", 0);
+        }
+        
+        /*if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.LeftArrow)) //droite
+        {
+            playerAnimator.SetInteger("Speed", 1);
+
+            if(Input.GetKey(KeyCode.RightArrow))
+            {
+                if (spriteRenderer.flipX)
+                {
+                    spriteRenderer.flipX = false;
+                }
+                transform.Translate(Vector2.right * Speed* Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow)) //gauche
+            {
+                if (!spriteRenderer.flipX)
+                {
+                    spriteRenderer.flipX = true;
+                }
+                transform.Translate(Vector2.left * Speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            playerAnimator.SetInteger("Speed", 0);
+        }*/
+
+       
+    }
+}
---
+++ Point'n'Click/Assets/RoutineScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoutineScript : MonoBehaviour {
+
+    public GameObject Vildrac;
+
+	// Use this for initialization
+	void Start () {
+        Vildrac.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.realtimeSinceStartup >= 3)
+        {
+            Vildrac.SetActive(true);
+        }
+    }
+}
