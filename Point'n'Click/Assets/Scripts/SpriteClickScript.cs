5ac45204fb80a79a6db34aa05584b0da5c0eeef5
Amarchy <axel.marchal@hotmail.fr>
2017-11-15 17:14:14 +0100

    Commit

--- Point'n'Click/Assets/Scripts/SpriteClickScript.cs
+++ Point'n'Click/Assets/Scripts/SpriteClickScript.cs
@@ -1,29 +1,13 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.EventSystems;
+﻿using UnityEngine;
 using Assets.Scripts;
 
 public class SpriteClickScript : MonoBehaviour {
 
     public GameObject sprite;
 
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void OnMouseDown()
     {
         Debug.Log(gameObject.name);
-        SharedObjects.SetInt("story", 1);
+        SharedObjects.SetInt(gameObject.name, 1);
     }
 }
