857db287ceff95d55b0aca6227e8caef1252c151
Amarchy <axel.marchal@hotmail.fr>
2017-12-06 09:32:13 +0100

    l

--- Point'n'Click/Assets/Scripts/SpriteClickScript.cs
+++ Point'n'Click/Assets/Scripts/SpriteClickScript.cs
@@ -8,6 +8,34 @@ public class SpriteClickScript : MonoBehaviour {
     public void OnMouseDown()
     {
         Debug.Log(gameObject.name);
-        SharedObjects.SetInt(gameObject.name, 1);
+        switch (sprite.name)
+        {
+            case "Choix 1":
+                {
+                    SharedObjects.SetInt(gameObject.name, 1);
+                    break;
+                }
+            case "Choix 2":
+                {
+                    SharedObjects.SetInt(gameObject.name, 2);
+                    break;
+                }
+            case "Choix 3":
+                {
+                    SharedObjects.SetInt(gameObject.name, 3);
+                    break;
+                }
+            default:
+                {
+                    SharedObjects.SetInt(gameObject.name, 0);
+                    break;
+                }
+        }
+        Debug.Log(SharedObjects.GetInt(gameObject.name));
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log(gameObject.name);
     }
 }
