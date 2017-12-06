9d6578d4089c9e415be7507d6c3a6bfc0cc378d3
Amarchy <axel.marchal@hotmail.fr>
2017-12-06 10:17:56 +0100

    Axel + Flo

---
+++ Point'n'Click2/Assets/Scripts/SpriteClickScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Assets.Scripts;
+
+public class SpriteClickScript : MonoBehaviour {
+
+    public GameObject sprite;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseDown()
+    {
+        Debug.Log(gameObject.name);
+        SharedObjects.SetInt("story", 1);
+    }
+}
